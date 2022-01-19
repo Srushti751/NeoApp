3fcfda6af43a13e7e162bf1be237f6d287727a1e
Srushti751 <90671944+Srushti751@users.noreply.github.com>
2022-01-19 09:48:13 +0530

    Update productModel.js

--- back-store/models/productModel.js
+++ back-store/models/productModel.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
 
 const productSchema = mongoose.Schema({
-    // "product_id": "5cfe3e81b4db0f338946eabc",
+   
     product_name: String,
     product_image: String,
     product_desc: String,
-    // product_rating: "3.34",
-    // product_producer: "Winchester Pvt. Ltd.",
+  
     product_cost: Number,
-    // category_id: Number,
+  
     category_id:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"category"
@@ -22,9 +21,7 @@ const productSchema = mongoose.Schema({
     images:Array,
     rating:Number
     
-    // product_stock: 443,
-    // product_dimension: "84 * 28",
-    // product_material: "Cobalt, Material",
+  
 
 
 },{timestamps:true})
